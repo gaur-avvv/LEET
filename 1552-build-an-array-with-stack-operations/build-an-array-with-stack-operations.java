31c46f5b7d5875c0262895f8d8358b23368b6abe
Gaurav Singh <87198156+gaur-avvv@users.noreply.github.com>
2026-06-25 23:20:43 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (84.63%) - LeetSync

---
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<String> buildArray(int[] target, int n) {
+        List<String> result = new ArrayList<>();
+        int targetIndex = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            if (targetIndex >= target.length) {
+                break;
+            }
+            
+            result.add("Push");
+            
+            if (i == target[targetIndex]) {
+                targetIndex++;
+            } else {
+                result.add("Pop");
+            }
+        }
+        
+        return result;
+    }
+}
